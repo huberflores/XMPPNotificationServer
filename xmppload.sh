2bc3b08c6c270381d6b168045995d37186edc94f
Huber Flores <huber@amatista.(none)>
2013-02-07 16:43:32 +0200

    Automatized message load

---
+++ xmppload.sh
@@ -0,0 +1,21 @@
+#This script must be run preferable within the application server side,
+#in order to avoid unnecessary latencies
+#./xmmpload.sh
+
+
+for t in {1..24}
+do
+   #Send 3 messages per minute, 1 message each 20s
+   for i in {1..3}
+   do
+     curl "http://localhost:8080/Notifier/xmppnotification?jidmobile=jidmobile@amazon-xmpp/Home&payload=OK"
+     sleep 20s
+   done
+   #Refresh rate 
+   sleep 1m #change  m=minutes, h=hours, d=days
+done
+
+
+
+
+
